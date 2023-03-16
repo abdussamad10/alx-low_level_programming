ef6864418c7701edf2ceb2ce9ff312ededcaddc2
abdussamad10 <abdessamadseffir@gmail.com>
2023-03-16 10:05:29 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
+ 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * main - prints "Programming is like building a multilingual puzzle"
+ * return 0.
+ */
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");

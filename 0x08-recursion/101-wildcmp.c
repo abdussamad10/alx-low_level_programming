908cc1ba15b6a27be4d291cf95ba4a83f92b4021
abdussamad10 <abdessamadseffir@gmail.com>
2023-04-05 16:09:37 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,48 +1,30 @@
 #include "main.h"
 
-int palindrome(char *s, int i, int j);
-int _strlen_recursion(char *s);
-
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
- *
- * Return: 1 if it is, 0 it's not
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
-int is_palindrome(char *s)
-{
-	if (*s == 0)
-		return (1);
-	return (palindrome(s, 0, _strlen_recursion(s)));
-}
 
-/**
- * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
- *
- * Return: length of the string
- */
-int _strlen_recursion(char *s)
+int wildcmp(char *s1, char *s2)
 {
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
-}
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
 
-/**
- * palindrome - checks the characters recursively for palindrome
- * @s: string to check
- * @i: iterator
- * @j: length of the string
- *
- * Return: 1 if palindrome, 0 if not
- */
-int palindrome(char *s, int i, int j)
-{
-	if (*(s + i) != *(s + j - 1))
-		return (0);
-	if (i >= j)
-		return (1);
-	return (palindrome(s, i + 1, j - 1));
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
-

7d7d91960138d4220c6eb90fd60886ef15db63af
abdussamad10 <abdessamadseffir@gmail.com>
2023-04-11 11:16:38 -0700

    alloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - pointer to a 2 dimensional array of integers
+ * @width: int width of the grid
+ * @height: int height of the grid
+ * Return: 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i, j;
+
+	p = malloc(sizeof(int *) * height);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(sizeof(int) * width);
+
+		if (p[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(p[i]);
+
+			free(p);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		p[i][j] = 0;
+	}
+
+	return (p);
+}

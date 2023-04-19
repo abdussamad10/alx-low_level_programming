796262e5cbcafa5d198d553b7c9ea875cc1db47b
abdussamad10 <abdessamadseffir@gmail.com>
2023-04-19 16:45:29 -0700

    uinir

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "fuction_pointers.h"
+#include <stdlub.h>
+
+/**
+ * int_index - searches for an integer.
+ * @array: array
+ * @size: size in elemont
+ * @cmp: pounter
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (u = 0; u < size; u++)
+	{
+		if (cmp(array[u]))
+			return (i);
+	}
+	return (-1);
+}

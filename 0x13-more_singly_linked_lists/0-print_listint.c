6f22668be2aff78a87cf59890ab65c92b54769e8
abdussamad10 <abdessamadseffir@gmail.com>
2023-04-30 07:07:45 -0700

    listin

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 
 /**
- * print_listint -  prints all the elements of a listint_t list.
+ * print_listint - prints all the elements of a listint_t list.
  * @h: pointer list
  * Return: the number of nodes
  */
 
 size_t print_listint(const listint_t *h)
 {
-	int a; 
-	a = 0;
+	int a = 0;
 
 	while (h)
 	{

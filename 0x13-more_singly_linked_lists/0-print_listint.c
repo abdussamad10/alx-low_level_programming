fa95da98012af044c72335a3033ad6a76c8984de
abdussamad10 <abdessamadseffir@gmail.com>
2023-04-30 06:58:51 -0700

    listin

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,3 @@
-#include "lists.h"
 #include <stdio.h>
 
 /**
@@ -9,7 +8,7 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int a = 0;
+	size_t a = 0;
 
 	while (h)
 	{
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,13 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 typedef struct listint_t
 {
         int n;
         struct listint_t *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);

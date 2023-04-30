3e929d4f70d683bf45e0eff26769f53bd616b1c3
abdussamad10 <abdessamadseffir@gmail.com>
2023-04-30 06:50:27 -0700

    listin

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint -  prints all the elements of a listint_t list.
@@ -8,8 +9,7 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int = a;
-	a = 0;
+	int a = 0;
 
 	while (h)
 	{

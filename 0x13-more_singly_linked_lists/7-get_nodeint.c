d4cfa35f301a6e6065404d2c05ca7b9f0f873355
abdussamad10 <abdessamadseffir@gmail.com>
2023-05-01 12:12:07 -0700

    get

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,7 +4,7 @@
  * get_nodeint_at_index - returns the nth node of a listint_t linked list.
  * @head: pointer list
  * @index: index of the node, starting at 0
- * Return; pinter
+ * Return: pinter
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {

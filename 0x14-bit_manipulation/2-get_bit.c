73d0a6463811edde70ac4d46d462df5c8a9cc44f
abdussamad10 <abdessamadseffir@gmail.com>
2023-05-04 13:08:43 -0700

    git bot

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,6 +2,7 @@
 /**
  * get_bit -  returns the value of a bit at a given index
  * @index: the index
+ * @n: number
  * Return: the value of the bit at index index or -1 if an error occure
  */
 int get_bit(unsigned long int n, unsigned int index)

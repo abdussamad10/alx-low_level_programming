d09cd1e009902754dc82704dc8a197bbffd3e895
abdussamad10 <abdessamadseffir@gmail.com>
2023-05-04 09:29:55 -0700

    BINARY

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,15 +7,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int num = strlen(b);
-	unsigned int i = 0;
-	
+	int sum = strlen(b);
+	unsigned int num = 0;
+	int i;
+
 	if (b == NULL)
         {
                 return (0);
         }
 
-	for (i = 0; i < num; i++)
+	for (i = 0; i < sum; i++)
 	{
 		if (b[i] == '0')
 		{
